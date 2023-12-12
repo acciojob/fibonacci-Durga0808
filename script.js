ab19d56242ffcd0e8b22f0382bc03e3a3b019bd4
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-13 00:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if(num==1)return 0;
+	if(num==2)return 1;
+
+	var a=0;
+	var b=1;
+	for(var i=3;i<=num;i++){
+		var c=a+b;
+		a=b;
+		b=c;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
